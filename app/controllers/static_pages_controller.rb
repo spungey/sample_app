27e2c395a922ea58d8fc13ff21a7c62d4b58b9dd
Eoin O Shea <eoin.oshea3@gmail.com>
2013-10-05 03:32:17 +0100

    swag

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,15 +3,7 @@ require 'twitter'
 class StaticPagesController < ApplicationController
   def home
     
-   Twitter.configure do |config|
-      config.consumer_key = "Uv2kKLGIxSUd9InVCNd3jA"
-      config.consumer_secret = "IKOg2VVV3crhqUU0bJqxI4qS6OXjulfCuITwZowX7zM"
-      config.oauth_token = "308867554-QDUi6DYmUeKEzaRLTaKadZhLg3vAneKY8XqctzEw"
-      config.oauth_token_secret = "NHwLCKXBvDijCFjxA2kLk7DnbHtBqqhDQ0Zb1kHjBw"
-    end
-    
-    #Twitter.update("I'm tweeting with @gem!")
-    Twitter.follow("gem")
+ 
   end
 
   def help
@@ -19,4 +11,24 @@ class StaticPagesController < ApplicationController
   
     def about
   end
+  
+  
+  def stocks
+    
+      Twitter.configure do |config|
+      config.consumer_key = "Uv2kKLGIxSUd9InVCNd3jA"
+      config.consumer_secret = "IKOg2VVV3crhqUU0bJqxI4qS6OXjulfCuITwZowX7zM"
+      config.oauth_token = "308867554-QDUi6DYmUeKEzaRLTaKadZhLg3vAneKY8XqctzEw"
+      config.oauth_token_secret = "NHwLCKXBvDijCFjxA2kLk7DnbHtBqqhDQ0Zb1kHjBw"
+    end
+    
+    #Twitter.update("I'm tweeting with @gem!")
+    Twitter.follow("stockwire24")
+    
+    Twitter.user_timeline("gem")
+    
+  end
+  
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ SampleApp::Application.routes.draw do
   # get "static_pages/contact"
   
 # match '/', to: 'static_pages#home', via: 'get'
- root  'static_pages#home'
+  root  'static_pages#stocks'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
