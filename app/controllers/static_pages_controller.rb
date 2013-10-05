f3cef71f74b45312be61e5c78dbc6e6de2ecf37a
Eoin O Shea <eoin.oshea3@gmail.com>
2013-10-05 03:23:25 +0100

    swag

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,8 @@ class StaticPagesController < ApplicationController
    Twitter.configure do |config|
       config.consumer_key = "Uv2kKLGIxSUd9InVCNd3jA"
       config.consumer_secret = "IKOg2VVV3crhqUU0bJqxI4qS6OXjulfCuITwZowX7zM"
-      config.oauth_token = "308867554-xxRlmIyYALknaFIa8ZOPtBZ8tUgCSmcunmcfQ7fn"
-      config.oauth_token_secret = "FmvXp3MoqoMWIoezMjZYBoTSB1jMvyhha9Jxb4OuAE"
+      config.oauth_token = "308867554-QDUi6DYmUeKEzaRLTaKadZhLg3vAneKY8XqctzEw"
+      config.oauth_token_secret = "NHwLCKXBvDijCFjxA2kLk7DnbHtBqqhDQ0Zb1kHjBw"
     end
     
     #Twitter.update("I'm tweeting with @gem!")
